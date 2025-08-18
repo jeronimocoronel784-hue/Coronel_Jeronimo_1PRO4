da2cb2de1f1eda6ae9faf1e1f6625c3cbfaa7a3b
jeronimocoronel784-hue <jeronimocoronel784@gmail.com>
2025-08-18 17:54:14 -0300

    Agrego ejercicios de Python del 1 al 16

--- ejerciciosComp.py
+++ ejerciciosComp.py
@@ -28,9 +28,46 @@ print("Por ejemplo, si le quiere aplicar un 25 porciento de descuento, el valor 
 descuento = float(input("Ingrese el valor de descuento que le quiere aplicar al producto: "))
 
 print("EJERCICIO 8: \n")
-precio_final = precio 
+precio_final = precio - (precio * descuento)
+print(f"El precio final con el descuento aplicado es: {precio_final}")
 
 print("EJERCICIO 9: \n")
+cadena = str(input("Ingrese una frase o texto: "))
 
+print("EJERCICIO 10: \n")
+longitud = len(cadena)
+print(f"La longitud de la cadena es: {longitud} caracteres")
 
+print("EJERCICIO 11: \n")
+precio = float(input("Ingrese un valor decimal para la variable precio: "))
+precio_entero = int(precio)
+print(f"El valor entero de la variable precio es: {precio_entero}")
 
+print("EJERCICIO 12: \n")
+nombre = str(input("Ingrese su nombre: "))
+apellido = str(input("Ingrese su apellido: "))
+nombre_completo = nombre + " " + apellido
+print(f"Su nombre compleot es: {nombre_completo}")
+
+print("EJERCICIO 13: \n")
+edad = int(input("Ingrese su edad: "))
+edad_mas_5 = edad + 5
+edad_menos_10 = edad - 10 
+print(f"Su edad incrementada en 5 es: {edad_mas_5}")
+print(f"Su edad disminuida en 10 es: {edad_menos_10}")
+
+print("EJERCICIO 14: \n")
+altura = float(input("Ingrese la altura en metros (Ejemplo 1.90): "))
+altura_cm = altura * 180
+resultado = (altura *4) / 3
+print(f"Su altura en centimetros es: {altura_cm} cm")
+print(f"Su altura multiplicada por 4 y luego dividida en 3 es: {resultado}")
+
+print("EJERCICIO 15: \n")
+nombre_mayus = str(input("Ingrese su nombre completo en mayusculas: "))
+nombre_minus = nombre_mayus.lower()
+print(f"Su nombre en minusculas es: {nombre_minus}")
+
+print("EJERCICIO 16: \n")
+nombre_mayuscula = nombre_mayus.capitalize()
+print(f"Su nombre con solo la primera letra en mayuscula es: {nombre_mayuscula}")
