539a336a755cceb6e98d47381dabfe41957a66ae
jeronimocoronel784-hue <jeronimocoronel784@gmail.com>
2025-08-18 09:40:24 -0300

    primer commit

---
+++ ejerciciosComp.py
@@ -0,0 +1,36 @@
+print("EJERCICIO 1: \n")
+numero1 = int(input("Ingrese un numero entero: "))
+
+print("EJERCICIO 2: \n")
+numero2 = float(input("Ingrese un numero decimal: "))
+
+print("EJERCICIO 3: \n")
+suma = numero1+numero2
+
+print("EJERCICIO 4: \n")
+resta = numero1-numero2
+multiplicacion = numero1*numero2
+division = numero1/numero2
+
+print(f"La suma de los dos numeros es: {suma}")
+print(f"La restas de los dos numeros es: {resta}")
+print(f"La multiplicacion de los dos numeros es: {multiplicacion}")
+print(f"La division de los dos numeros es: {division}")
+
+print("EJERCICIO 5: \n")
+nombre = str(input("Ingrese su nombre: "))
+
+print("EJERCICIO 6: \n")
+precio = float(input("Ingrese el precio del articulo: "))
+
+print("EJERCICIO 7: \n")
+print("Por ejemplo, si le quiere aplicar un 25 porciento de descuento, el valor a ingresar seria 0,25 \n")
+descuento = float(input("Ingrese el valor de descuento que le quiere aplicar al producto: "))
+
+print("EJERCICIO 8: \n")
+precio_final = precio 
+
+print("EJERCICIO 9: \n")
+
+
+
